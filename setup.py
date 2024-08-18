e1aeb40f75a2623e36ff3483c5d502c39e7938a8
Yuki5155 <yukiasano55@gmail.com>
2024-08-18 19:08:50 +0000

    ライブラリ化

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="notion-api",
+    version="0.0.1",
+    author="Yuki Asano",
+    author_email="yukiasano55@gmail.com",
+    description="A short description of your package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yuki5155/notion-api",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        # リストに依存パッケージを追加
+        # 例: 'requests>=2.22.0',
+    ],
+)
